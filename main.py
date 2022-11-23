fe6c7cbc19221ddcc91901359a9c6d15a6174509
maor weiss <maorweissb@gmail.com>
2022-11-23 20:51:31 +0200

    Create a CSV file #3 - Creating youtube_dl_install functions for every os

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 import csv
 import os
 import sys
+import platform
 
 file_name = './songs_links.csv'
 
@@ -10,6 +11,28 @@ def csv_creation():
     with open('songs_links.csv', 'w', newline='') as csv_file:
         writer = csv.writer(csv_file)
         writer.writerow(["Song Name", "Author", "Youtube Link", "Status"])
+
+
+def youtube_dl_install():
+    # If OS is Windows - Do a Windows Installation
+    if platform.system == 'Windows':
+        windows_youtube_dl_install()
+    elif platform.system == 'Linux':
+        windows_youtube_dl_install()
+    elif platform.system == 'Darwin':
+        windows_youtube_dl_install()
+
+
+def windows_youtube_dl_install():
+    pass
+
+
+def linux_youtube_dl_install():
+    pass
+
+
+def mac_youtube_dl_install():
+    pass
 
 
 def isFiedsEmpty(fileName):
@@ -25,8 +48,14 @@ def isFiedsEmpty(fileName):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
+    #Todo - Check if there's youtube_dl pip pkg installed
+
+    # If there's no youtube_dl pip pkg - Install it
+    youtube_dl_install()
+
     # Check if csv file exists and its values
     if not os.path.isfile(file_name):
         csv_creation()
         print("Please fill the 'songs_links.csv' fields")
         sys.exit()
+
